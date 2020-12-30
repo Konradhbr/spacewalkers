61c65afd5148a6c4a10f12685814f7ad295ac4ec
@konradhbr <konradhbr@gmail.com>
2020-12-30 11:18:18 +0100

    update

--- src/js/index.js
+++ src/js/index.js
@@ -1,37 +1,63 @@
 function getValues() {
-	var userName = document.getElementById("name").value;
-	var userCompany = document.getElementById("company").value;
-	var userPhoneNumber = document.getElementById("mobile-number").value;
-	var userEmail = document.getElementById("email").value;
-	var userMessage = document.getElementById("message").value;	
+  var userName = document.getElementById('name').value;
+  var userCompany = document.getElementById('company').value;
+  var userPhoneNumber = document.getElementById('mobile-number').value;
+  var userEmail = document.getElementById('email').value;
+  var userMessage = document.getElementById('message').value;
 }
 
-function disappear() {
-	var email = document.getElementById('email').value;
-	var placeholder = document.getElementById('holderReq');
-	if (email != " ") {
-		placeholder.style.display = 'none';
-	}
-	else {}
-}
+$(document).ready(function () {
+  $('.input-placeholder input').on('blur input', function () {
+    if ($(this).val() != '') {
+      $(this).next('.placeholder').hide();
+    } else {
+      $(this).next('.placeholder').show();
+    }
+  });
+});
 
-// $('#form')
-//     .ajaxForm({
-//         url : 'index.html', // or whatever
-//         dataType : 'submit',
-//         success : function (response) {
-//             alert("The server says: " + response);
-//         }
-//     })
-// 	;
+$(document).ready(function () {
+  $('form').on('submit', function (event) {
+    event.preventDefault();
 
-function qwe() {
-	window.location.replace('https://www.youtube.com/watch?v=CkLSapxVyGQ');
-}
-// function sendForm() {
-// 	axios.post(`/api/newsletter/signup`, { email: this.email }).then(res => {
-// 		if (res.status === 200) {
-// 			this.success = true;
-// 		}
-// 	});
-// },
+    var formValues = $(this).serialize();
+
+    $.ajax({
+      type: 'POST',
+      url: 'https://tecra.space/api/spacewalkers/training',
+      crossDomain: true,
+      data: formValues,
+      dataType: 'json',
+      success: function (responseData, textStatus, jqXHR) {
+        console.log(responseData);
+        swal
+          .fire({
+            title: 'Formularz wysłany',
+            text:
+              'Zgłoszenie zostało przyjęte - proszę oczekiwać kontaktu z naszej strony.',
+            icon: 'success',
+            // confirmButtonText:
+            //   '<a style="text-decoration: none; color: white;" href="https://spacewalkersgames.com/">OK</a>',
+          })
+          .then(function () {
+            location.reload();
+          });
+      },
+      error: function (responseData, textStatus, errorThrown) {
+        // alert('POST failed.');
+        swal
+          .fire({
+            title: 'Formularz wysłany',
+            text:
+              'Zgłoszenie zostało przyjęte - proszę oczekiwać kontaktu z naszej strony.',
+            icon: 'success',
+            // confirmButtonText:
+            //   '<a style="text-decoration: none; color: white;" href="https://spacewalkersgames.com/">OK</a>',
+          })
+          .then(function () {
+            location.reload();
+          });
+      },
+    });
+  });
+});
